eebafbc4536719bd72c0620ec69e893aff454c09
Akshay Kumar <email.akshay98@gmail.com>
2019-05-04 17:43:24 +0530

    add local mongodb

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express();
-const PORT=5000
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const PORT = process.env.PORT || 5000;
 var airQualitySensor = require('./sensors/air-quality-sensor.js')
 var temperatureSensor = require('./sensors/temperature-sensor.js')
 var vibrationsSensor = require('./sensors/vibrations-sensor.js')
+
+/*********************DATABASE VARIABLES*****************************/
+var db
+
+var uri = 'mongodb://localhost:27017/sensorDatabase';
+const dbName = 'sensorApi';
+const client = new MongoClient(uri, { useNewUrlParser: true });
+/********************************************************************/
+
+/*******************TEMPERATURE TEMPORARY VARIABLES******************/
+var currentTemperature
+var currentHumidity
+/********************************************************************/
+
+/*******************AIR QUALITY TEMPORARY VARIABLES******************/
+var currentCO2Level
+var currentO2Level
+/********************************************************************/
+
 
 app.get('/sendAirQuality', async function(req, res) {
     console.log("send Air Quality endpoint hit")
@@ -17,14 +39,27 @@ app.get('/sendAirQuality', async function(req, res) {
 });
 
 app.get('/sendTemperature', async function(req, res) {
+    currentTemperature = req.query.temp;
+    currentHumidity = req.query.hum;
     console.log("send Temperature endpoint hit")
     console.log("The temperature is " + req.query.temp);
     console.log("The humidity is:- ", req.query.hum);
-    var response = await temperatureSensor.storeTemperature();
+    var response = await temperatureSensor.storeTemperature(db, currentTemperature, currentHumidity);
     if(response == true) {
         console.log("store successfull");
     } else {
         console.log("unsuccessful");
+    }
+    return res.send(response);
+});
+
+app.get('/getTemperature', async function(req, res) {
+    var response = await temperatureSensor.findTemperature(db);
+    if(response == null) {
+        console.log("find unsuccessfull");
+    } else {
+        console.log(response);
+        console.log("find successful");
     }
     return res.send(response);
 });
@@ -41,7 +76,14 @@ app.get('/sendVibrations', async function(req, res) {
 });
 
 app.listen(process.env.PORT || PORT, ()=> {
-    console.log("##########################Listening to port 5000###########################")
+    console.log("##########################Listening to port ${PORT}###########################")
+    MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+        if(err) {
+             console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+
+        }    
+        assert.equal(null, err);
+        console.log("Connected successfully to database server");  
+        db = client.db(dbName);
+    });
 })
-
-
