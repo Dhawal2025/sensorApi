23acf8997c4ac4530b7a98b3639c3fe290669aeb
Sagar Kalra <sagar.kalra98@hotmail.com>
2019-07-09 15:36:56 +0530

    add vibrations graph

--- frontend/src/SensorGaugeMeters/Vibrations.js
+++ frontend/src/SensorGaugeMeters/Vibrations.js
@@ -1,57 +1,115 @@
 import React, { Component } from 'react';
-import RTChart from 'react-rt-chart';
-import '../css/c3.css';
-import Axios from 'axios';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  VerticalRectSeries
+} from 'react-vis';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+const location = window.location.host;
+const client = new W3CWebSocket(`ws://172.16.168.29:5000/echo?connectionType=client`);
 
+const DATA = [
+//   {x0: ONE_DAY * 2, x: ONE_DAY * 3, y: 1},
+//   {x0: ONE_DAY * 7, x: ONE_DAY * 8, y: 1},
+//   {x0: ONE_DAY * 8, x: ONE_DAY * 9, y: 1},
+//   {x0: ONE_DAY * 9, x: ONE_DAY * 10, y: 2},
+//   {x0: ONE_DAY * 10, x: ONE_DAY * 11, y: 2.2},
+//   {x0: ONE_DAY * 19, x: ONE_DAY * 20, y: 1},
+//   {x0: ONE_DAY * 20, x: ONE_DAY * 21, y: 2.5},
+//   {x0: ONE_DAY * 21, x: ONE_DAY * 24, y: 1}
+    {x0: 0, x: 1, y: 1},
+    {x0: 2, x: 3, y: 2},
+    {x0: 7, x: 3, y: 7}
+].map(el => ({x0: el.x0, x: el.x, y: el.y}));
 class Vibrations extends Component {
     
     constructor(props) {
         super(props);
-        this.state = { y: 0, x: 0, z: 0 };
+        this.state = {  vibrations: [], 
+                        minX: 0, 
+                        minY: 0,
+                        maxX: 10,
+                        maxY: 10  
+                    };
     }
 
     componentDidMount() {
         const min = 4;
         const max = 6;
-        // setInterval( () => Axios.get('/getCurrentVibrations').then(res =>{
-        //     this.setState({ 
-        //         x: res.data.currentX,
-        //         y: res.data.currentY,
-        //         z: res.data.currentZ
-        //     })
-        // }), 1000)
-        // setInterval(() => this.setState({ x: Math.floor(Math.random()*(max-min+1)+min), y: Math.floor(Math.random()*(max-min+1)+min), z: Math.floor(Math.random()*(max-min+1)+min) }), 1000)
+        const serverData = [1, 4, 5, 6, 19];
+                    var temp = [];
+                    for (var obj in serverData) {
+                        if (parseInt(obj) + 1 > this.state.maxX) this.setState({ maxX: parseInt(obj) + 2 });
+                        if (serverData[obj] > this.state.maxY) this.setState({ maxY: serverData[obj] + 1 });
+                        console.log(obj);
+                        const tempDict = {x0: parseInt(obj), x: parseFloat(obj) + 0.9, y: serverData[obj]};
+                        console.log(tempDict);
+                        temp.push(tempDict);
+                        console.log(temp[temp.length - 1]);
+                                         
+                    }
+                    console.log(temp, "TEMP");
+                    
+                    this.setState({vibrations: temp});
+                    
+        try {
+            client.onopen = () => {
+                console.log('Vibrations WebSocket Client Connected');
+                console.log(window.location);
+                console.log(this.state.vibrations, "RAW Data");
+                
+            };
+            client.onmessage = (message) => {
+               const json = JSON.parse(message.data);
+                
+                if(json.sensorType == this.props.sensorType) {
+                    console.log(window.location.href);
+                    console.log(json, "Vibrations JSON");
+                    console.log(json.data.currentVibration);
+                    const serverData = json.data.currentVibration;
+                    const temp = [];
+                    for (const obj in serverData) {
+                        if (parseInt(obj) + 1 > this.state.maxX) this.setState({ maxX: parseInt(obj) + 2 });
+                        if (serverData[obj] > this.state.maxY) this.setState({ maxY: serverData[obj] });
+                        const tempDict = {x0: obj, x: parseFloat(obj) + 0.9 , y: serverData[obj]};
+                        temp.push(tempDict);                 
+                    }
+                    this.setState({vibrations: temp});
+                }
+            };
+        } catch(error) {
+            console.log(error);
+            
+        }    
     }
-    render() {
-        var data = {
-            date: new Date(),
-            y_axis: this.state.y,
-            x_axis: this.state.x,
-            z_axis: this.state.z
-          };
-        var chart = {
-            size: {
-                height: '300'
-            },
-            margin: {
-                top: 50,
-                right: 50
-            },
-            axis: {
-                y: { min: -3, max: 3 }
-            },
-            point: {
-                show: false
-            },
-        };
-        return (
-            <RTChart
-            chart = {chart}
-            fields={['x_axis', 'y_axis', 'z_axis']}
-            maxValues={10}
-            data={data} />
-        );
-    }
+render () {
+    console.log(this.state);
+    return (
+        <XYPlot
+          xDomain={[this.state.minX, this.state.maxX]}
+          yDomain={[this.state.minY, this.state.maxY]}
+          width={500}
+          height={300}
+        >
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis style={{
+            line: {stroke: '#ADDDE1'},
+            ticks: {stroke: '#ADDDE1'},
+            text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+        }}/>
+        <YAxis style={{
+            line: {stroke: '#ADDDE1'},
+            ticks: {stroke: '#ADDDE1'},
+            text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
+        }}/>
+          <VerticalRectSeries data={this.state.vibrations} style={{stroke: '#000'}} />
+        </XYPlot>
+    );
+}
 }
 
 export default Vibrations;
